39115f5de2a55e6b019bd90a374ae4435db1de79
Trong <gnortpro4@gmail.com>
2019-03-30 21:09:13 +0700

    contact

--- js/custom.js
+++ js/custom.js
@@ -33,3 +33,26 @@ $(document).ready(function () {
  
   })(jQuery);
 });
+
+
+// Form
+jQuery(".floatlabel input").focus(function() {
+  var elementId = $(this).attr('id');
+   jQuery("label[for=" + elementId + "]").addClass('floatlabel-shift');
+});
+jQuery(".floatlabel input").blur(function() {
+   if($(this).val().length == 0) {
+    var elementId = $(this).attr('id');
+     jQuery("label[for=" + elementId + "]").removeClass('floatlabel-shift');
+   }
+});
+jQuery(".floatlabel input").focus(function() {
+  var elementId = $(this).attr('id');
+   jQuery("label[for=" + elementId + "]").addClass('floatlabel-active');
+});
+jQuery(".floatlabel input").blur(function() {
+  var elementId = $(this).attr('id');
+   jQuery("label[for=" + elementId + "]").removeClass('floatlabel-active');
+});
+
+
